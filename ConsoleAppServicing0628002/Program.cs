d11dd66e0de78496891ff9beb0eaae5d0655a118
Davi Ji <v-weji1@microsoft.com>
2024-06-28 06:11:56 +0000

    Add project files.

---
+++ ConsoleAppServicing0628002/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppServicing0628002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
